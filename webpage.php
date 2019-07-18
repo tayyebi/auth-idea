ebf4ca75bbd8bd9c61b55edfb43611b9f14b60ca
MohammadReza Tayyebi <tayyebimohammadreza@gmail.com>
2019-07-18 22:02:53 +0430

    Init

---
+++ webpage.php
@@ -0,0 +1,41 @@
+<!--
+
+// ========================
+// Webpage which requires auth
+// ========================
+
+-->
+
+<pre>
+<code>
+<?php
+
+// consts
+$auth_server_address = "http://192.168.1.7/auth-idea";
+$token = '';
+
+// Get request headers
+foreach (getallheaders() as $name => $value) { 
+    echo "$name: $value <br>";
+    if ($name == "Auth-Token")
+        $token = $value;
+} 
+?> 
+</code>
+</pre>
+
+
+<?php
+// check token with auth server
+$get_file_content = file_get_contents("$auth_server_address/auth.php?token=$token");
+if ($get_file_content == "token valid:false")
+{
+    // invalid token
+    echo "Login expired...!";
+    exit;
+}
+?>
+
+
+<!-- page private content -->
+<img src="tenor.gif">
